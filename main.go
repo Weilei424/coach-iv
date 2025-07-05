31fd333d8a4b61aebbb3118cda033e95d26e11ef
Mason Wang <mason123wp@gmail.com>
2025-07-05 03:29:19 -0400

    add initial implementation of Discord bot with message handling

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	token := os.Getenv("DISCORD_TOKEN")
+	if token == "" {
+		log.Fatal("DISCORD_TOKEN environment variable is required")
+	}
+
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal("Error creating Discord session:", err)
+	}
+
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		log.Fatal("Error opening connection:", err)
+	}
+
+	log.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.Contains(strings.ToLower(m.Content), "<@"+s.State.User.ID+">") {
+		s.ChannelMessageSend(m.ChannelID, "Hello! I'm here and ready to help! 👋")
+	}
+}
